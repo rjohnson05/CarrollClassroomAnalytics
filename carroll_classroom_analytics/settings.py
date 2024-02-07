a0fe3e3a663e68791dbb056d6fd2516289a7d135
Adrian Rincon <arincon-jimenez@carroll.edu>
2024-02-07 12:49:25 -0700

    connected mysql database

--- carroll_classroom_analytics/settings.py
+++ carroll_classroom_analytics/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'carroll_classroom_analytics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carroll_classrooms',
+        'USER': 'root',
+        'PASSWORD': 'Comfypillow01*',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
